e7a657389562011e18d86ac0b2391d8e9862596c
Jalbert2010 <e.b.j.albert@gmail.com>
2023-09-06 17:48:50 -0500

    Javascript

---
+++ CarpetaHenry/operadores.js
@@ -0,0 +1,23 @@
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Potencia
+console.log(2**3);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
+
+//Adicion String
+console.log('Hola' + ' ' + 'Henry');
